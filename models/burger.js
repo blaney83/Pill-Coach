2789e7da3bef110d31c30a8ba9cf0e51acd761a2
Carrington Rain Allison <Carrington_Allison@yahoo.com>
2018-11-19 11:39:09 -0700

    working start

--- models/burger.js
+++
@@ -1,9 +0,0 @@
-//! This is just an example model function to access table in database.
-
-// module.exports = function(sequelize, DataTypes) {
-//   let Burger = sequelize.define("Burger", {
-//     burger_name: DataTypes.STRING,
-//     devoured: DataTypes.BOOLEAN
-//   });
-//   return Burger;
-// };
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -50,4 +50,4 @@ module.exports = function (app) {
 //           res.json(dbBurger);
 //         });
 //       });
-// };
+};
