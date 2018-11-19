9d455fa5512dbcc1a3649c30e971dc7cda963975
Justin.B.Brown <brownee06@gmail.com>
2018-11-19 12:52:02 -0700

    Add .gitignore, passports adding to db, not rendering hbrs yet

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 let express = require("express");
+let session = require("express-session");
+let passport = require("./config/passport");
 
 let app = express();
 let PORT = process.env.PORT || 8080;
@@ -12,6 +14,11 @@ app.use(express.json());
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
+// Set passport
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Set Handlebars.
 let exphbs = require("express-handlebars");
 
@@ -20,10 +27,11 @@ app.set("view engine", "handlebars");
 
 // Routes
 require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
 
-db.sequelize.sync().then(function () {
-  app.listen(PORT, function () {
-    console.log("Server listening on: http://localhost:" + PORT);
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
 });
