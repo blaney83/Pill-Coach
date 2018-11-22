b9f99fb37b82ac9798521d32921c50a9039d7798
Blaney83 <professionallaney@gmail.com>
2018-11-21 22:34:08 -0700

    Build smart link building function to create dynamic links to external content, regardless of the condition of data being linked. Implemented for related medicines and conditions.

--- public/assets/js/html.js
+++ public/assets/js/html.js
@@ -3,39 +3,77 @@
 $(document).ready(function () {
     console.log("hello")
 
-    $(document).on("click", ".tile", function(event){
-        $("#exampleModalLong").modal("toggle")
-        let rx = event.target.id;
-        console.log(rx)
+    let dataSelectors = [".tile"
+        //can add modal information to other things by putting their selectors here
+    ];
 
-        $.ajax({
-            method: "GET",
-            url: "/getinfo",
-            data: {
-                key: rx,
-            },
-        }).then(function (resp) {
-            console.log(resp)
-            let precautions = "<h3>Precautions</h3><h5>Never take this medication with the following: </h5>" + resp.generalInfo.doNotUseWith + "<h5>The presence of other medical problems may affect the use of this medicine. Make sure you tell your doctor if you have any other medical problems, especially: </h5>" + parseArrayIntoUnorderedList(resp.generalInfo.precautionMedicalConditions) + "<h5>FDA Warning Label: </h5>" + resp.infoBlackBox
-            let sideEffectsBuild = "<h4>" + resp.sideEffects.sideEffectsDisclaimer + "</h4>" + resp.sideEffects.sideEffectsLists.join("")
-            $("#sendPrecautionsHere").html(precautions);
-            $("#sendRelatedDrugsHere").html(parseArrayIntoUnorderedList(resp.generalInfo.infoRelatedPills));
-            $("#sendDosingHere").html(resp.generalInfo.dosingInfoParsed);
-            $("#sendOverviewHere").html(resp.generalInfo.overviewContent);
-            $("#sendWhatToWatchForHere").text(resp.sideEffects.whatToWatchForSummary)
-            $("#sendSideEffectsHere").html(sideEffectsBuild);
-            
-            function parseArrayIntoUnorderedList(array){
-                let starterArray = ["<ul>"]
-                array.forEach(function(val){
-                    let listItem = "<li>" + val + "</li>"
-                    starterArray.push(listItem)
-                });
-                starterArray.push("</ul>")
-                let returnString = starterArray.join("")
-                return(returnString);
-            }
-        })
+    $(document).on("click", ".tile", function (event) {
+        $("#exampleModalLong").modal("toggle")
     })
+
+    function getData(arr) {
+        $(document).on("click", arr, function (event) {
+            let rx = event.target.id;
+            console.log(rx)
+
+            $.ajax({
+                method: "GET",
+                url: "/getinfo",
+                data: {
+                    key: rx,
+                },
+            }).then(function (resp) {
+                console.log(resp)
+                let precautions = "<h3>Precautions</h3><h5>Never take this medication with the following: </h5>" + resp.generalInfo.doNotUseWith + "<h5>The presence of other medical problems may affect the use of this medicine. Make sure you tell your doctor if you have any other medical problems, especially: </h5>" + unorderedListSmartLinks(resp.generalInfo.precautionMedicalConditions) + "<h5>FDA Warning Label: </h5>" + resp.infoBlackBox
+                let sideEffectsBuild = "<h4>" + resp.sideEffects.sideEffectsDisclaimer + "</h4>" + resp.sideEffects.sideEffectsLists.join("")
+                $("#sendPrecautionsHere").html(precautions);
+                $("#sendRelatedDrugsHere").html(unorderedListSmartLinks(resp.generalInfo.infoRelatedPills));
+                $("#sendDosingHere").html(resp.generalInfo.dosingInfoParsed);
+                $("#sendOverviewHere").html(resp.generalInfo.overviewContent);
+                $("#sendWhatToWatchForHere").text(resp.sideEffects.whatToWatchForSummary)
+                $("#sendSideEffectsHere").html(sideEffectsBuild);
+
+                function unorderedListSmartLinks(array) {
+                    let starterArray = ["<ul>"]
+                    array.forEach(function (val) {
+                        let valArr = val.split("");
+                        let urlSnip = []
+                        let testInd = valArr.findIndex(checkPunctuation)
+                        let linkTag = []
+                        if (testInd > 0) {
+                            linkTag.push(valArr.slice(0, testInd).join(""))
+                        }else{
+                            linkTag.push(valArr.join(""))
+                        }
+                        let arrArr = linkTag[0].split("")
+                        arrArr.forEach((char) => {
+                            if (char === " ") {
+                                let newChar = "_"
+                                urlSnip.push(newChar)
+                            } else {
+                                urlSnip.push(char)
+                            }
+                        })
+                        let builtUrl = "https://en.wikipedia.org/wiki/" + urlSnip.join("")
+                        let listItem = '<li><a href="' + builtUrl + '">' + val + '</a></li>'
+                        starterArray.push(listItem)
+                    });
+                    starterArray.push("</ul>")
+                    let returnString = starterArray.join("")
+                    return (returnString);
+                }
+            })
+        })
+    };
+
+    dataSelectors.forEach(sel => {
+        getData(sel);
+    });
+
+    function checkPunctuation(val) {
+        if(val == ","|| val== "("|| val=="["){
+            return true;
+        }
+    }
 })
 //current code for Modal ^^^^^^^^^^^^^^^^^^^^^
