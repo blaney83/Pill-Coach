f267be016fadc77ba32611f6fda750d34a214031
Justin.B.Brown <brownee06@gmail.com>
2018-11-24 15:25:53 -0700

    Comment out .hasMany due to errors

--- models/user.js
+++ models/user.js
@@ -29,12 +29,12 @@ module.exports = function(sequelize, DataTypes) {
     user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
   });
 
-  User.associate = function(models) {
-    // Associating Author with Posts
-    // When an Author is deleted, also delete any associated Posts
-    User.hasMany(models.Pill, {
-      onDelete: "cascade"
-    });
-  };
+  // User.associate = function(models) {
+  //   // Associating Author with Posts
+  //   // When an Author is deleted, also delete any associated Posts
+  //   User.hasMany(models.Pill, {
+  //     onDelete: "cascade"
+  //   });
+  // };
   return User;
 };
