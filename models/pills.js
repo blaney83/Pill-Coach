f7edef2dad9159717112faa34dcd667cb7558240
Justin.B.Brown <brownee06@gmail.com>
2018-11-24 01:48:55 -0700

    Add association between users and pills

--- models/pills.js
+++ models/pills.js
@@ -43,5 +43,15 @@ module.exports = function (sequelize, DataTypes) {
         {
             timestamps: false
         });
+
+        Pill.associate = function(models) {
+            // We're saying that a Post should belong to an Author
+            // A Post can't be created without an Author due to the foreign key constraint
+            Pill.belongsTo(models.User, {
+              foreignKey: {
+                allowNull: false
+              }
+            });
+          };
     return Pill;
 };
